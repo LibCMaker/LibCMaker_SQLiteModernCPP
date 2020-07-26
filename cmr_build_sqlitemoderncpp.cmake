06abc948cbd2a319d7dde32b14b4c19c16e4c817
NikitaFeodonit <nfeodonit@yandex.com>
2020-07-26 17:40:14 +0300

    Use CACHE vars in cmr_build_*.cmake

--- cmr_build_sqlitemoderncpp.cmake
+++ cmr_build_sqlitemoderncpp.cmake
@@ -29,9 +29,15 @@
 # Lib's name, version, paths
 #-----------------------------------------------------------------------
 
-set(SQLITEMODERNCPP_lib_NAME     "SQLiteModernCPP")
-set(SQLITEMODERNCPP_lib_VERSION  "3.2.20190217")
-set(SQLITEMODERNCPP_lib_DIR      "${CMAKE_CURRENT_LIST_DIR}")
+set(SQLITEMODERNCPP_lib_NAME "SQLiteModernCPP")
+set(
+  SQLITEMODERNCPP_lib_VERSION "3.2.20190217"
+  CACHE STRING "SQLITEMODERNCPP_lib_VERSION"
+)
+set(
+  SQLITEMODERNCPP_lib_DIR "${CMAKE_CURRENT_LIST_DIR}"
+  CACHE PATH "SQLITEMODERNCPP_lib_DIR"
+)
 
 # To use our Find<LibName>.cmake.
 list(APPEND CMAKE_MODULE_PATH "${SQLITEMODERNCPP_lib_DIR}/cmake/modules")
@@ -41,7 +47,11 @@ list(APPEND CMAKE_MODULE_PATH "${SQLITEMODERNCPP_lib_DIR}/cmake/modules")
 # LibCMaker_<LibName> specific vars and options
 #-----------------------------------------------------------------------
 
-set(COPY_SQLITEMODERNCPP_CMAKE_BUILD_SCRIPTS ON)
+option(
+  COPY_SQLITEMODERNCPP_CMAKE_BUILD_SCRIPTS
+  "COPY_SQLITEMODERNCPP_CMAKE_BUILD_SCRIPTS"
+  ON
+)
 
 
 #-----------------------------------------------------------------------
